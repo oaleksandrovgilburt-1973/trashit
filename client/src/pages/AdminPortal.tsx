c46a067e7eb394dee2ef63160a16eaf272918dbe
oaleksandrovgilburt-1973 <o.aleksandrov.gilburt@gmail.com>
2026-07-07 11:33:35 +0300

    A

--- client/src/pages/AdminPortal.tsx
+++ client/src/pages/AdminPortal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useLocation } from "wouter";
 import { trpc } from "@/lib/trpc";
 import { Button } from "@/components/ui/button";
