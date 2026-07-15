f3895b1ec72b24a90d019d1d886cad49e1b69ce8
oaleksandrovgilburt-1973 <o.aleksandrov.gilburt@gmail.com>
2026-07-15 14:35:35 +0300

    Subscription

--- client/src/pages/Subscription.tsx
+++ client/src/pages/Subscription.tsx
@@ -298,6 +298,7 @@ export default function Subscription() {
               ))}
             </div>
           </div>
+          </div>
         ) : (
           /* New subscription form */
           <div className="space-y-5">
