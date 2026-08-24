3e483f09a1e74b9a27191481d34c8a5ddd75e747
oaleksandrovgilburt-1973 <o.aleksandrov.gilburt@gmail.com>
2026-08-24 12:58:00 +0300

    Add Android/iOS location permissions, native Google Sign-In for Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -11,6 +11,11 @@ const config: CapacitorConfig = {
   plugins: {
     CapacitorHttp: {
       enabled: false
+    },
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '1007790802752-qvc11eo6iuh6mvt3vhkmluqm2adhu9b8.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true
     }
   }
 };
