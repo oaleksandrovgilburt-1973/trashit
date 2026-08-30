500725b66aa310b02b2e714d640b06c04b9391e1
oaleksandrovgilburt-1973 <o.aleksandrov.gilburt@gmail.com>
2026-08-30 17:11:00 +0300

    Force fresh build (cache-bust)

--- server/routers.ts
+++ server/routers.ts
@@ -1,4 +1,5 @@
-﻿import { TRPCError } from "@trpc/server";
+﻿
+import { TRPCError } from "@trpc/server";
 import { jwtVerify, createRemoteJWKSet } from "jose";
 import { z } from "zod";
 import { subscriptionVisits } from "../drizzle/schema";
